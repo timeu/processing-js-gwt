a5f4ab268d235734fb6123b02b1808cf81ff32f7
Uemit Seren <uemit.seren@gmail.com>
2015-08-11 15:30:56 +0200

    Added Javadoc comments

--- processingjs-gwt/src/main/java/com/github/timeu/gwtlibs/processingjsgwt/client/Processing.java
+++ processingjs-gwt/src/main/java/com/github/timeu/gwtlibs/processingjsgwt/client/Processing.java
@@ -13,10 +13,15 @@ import com.google.gwt.resources.client.ExternalTextResource;
 import com.google.gwt.resources.client.ResourceCallback;
 import com.google.gwt.resources.client.ResourceException;
 import com.google.gwt.resources.client.TextResource;
+import com.google.gwt.safehtml.shared.SafeUri;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Widget;
 
-
+/**
+ * Processing wrapper widget around a ProcessingJS sketch
+ *
+ * @param <I> This must be an interface or class that extends/implements {@link ProcessingInstance} and is annotated with @JsType
+ */
 public class Processing <I extends ProcessingInstance> extends Widget  {
 
 	interface ProcessingClientBundle extends ClientBundle {
@@ -26,21 +31,29 @@ public class Processing <I extends ProcessingInstance> extends Widget  {
 		TextResource processingjs();
 	}
 	
-	protected String url;
 	protected I pInstance;
 	protected boolean isLoaded = false;
 	protected final Canvas canvas;
-	
+
+    /**
+     * Creates a Processing widget
+     */
 	public Processing() {
 		injectScript();
 		canvas = Canvas.createIfSupported();
 		setElement(canvas.getElement());
 	}
 
-	public void loadFromUrl(String url,final Runnable onLoad) throws RequestException 
+    /**
+     * Loads the ProcessingJS sketch from a URL.
+     *
+     * @param url URL to the ProcessingJS sketch
+     * @param onLoad optional callback that is executed when the sketch is loaded
+     * @throws RequestException if the request to the URL throws an Exception
+     */
+	public void load(SafeUri url,final Runnable onLoad) throws RequestException
 	{
-		this.url = url;
-		RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, url);
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, url.asString());
 		builder.setCallback(new RequestCallback() {
 			
 			@Override
@@ -56,6 +69,12 @@ public class Processing <I extends ProcessingInstance> extends Widget  {
 		builder.send();
 	}
 
+    /**
+     * Loads a ProcessingJS sketch from an {@link ExternalTextResource}
+     *
+     * @param code the code of the ProcessingJS sketch
+     * @param onLoad optional callback that is executed when the sketch is loaded
+     */
 	public void load(ExternalTextResource code ,final Runnable onLoad) {
 		try
 		{
@@ -73,7 +92,13 @@ public class Processing <I extends ProcessingInstance> extends Widget  {
 		}
 		catch (Exception e) {}
 	}
-	
+
+    /**
+     * Loads a ProcessingJS sketch from a String
+     *
+     * @param code the code of the ProcessingJS sketch
+     * @param onLoad optional callback that is executed when the sketch is loaded
+     */
 	public void load(String code,final Runnable onLoad)
 	{
 		pInstance = init(code,getElement());
@@ -81,12 +106,23 @@ public class Processing <I extends ProcessingInstance> extends Widget  {
 		if (onLoad != null)
 			onLoad.run();
 	}
-	
+
+    /**
+     * Provides access to the (custom) ProcessingInstance that allows the user
+     * to interface with the ProcessingJS sketch
+     *
+     * @return the actual {@link ProcessingInstance} instance
+     */
 	public I getInstance()
 	{
 		return pInstance;
 	}
-	
+
+    /**
+     * Can be used to check if the sketch is loaded
+     *
+     * @return true if the sketch is loaded
+     */
 	public boolean isLoaded()
 	{
 		return isLoaded;
@@ -96,8 +132,13 @@ public class Processing <I extends ProcessingInstance> extends Widget  {
 	protected final native I init(String programm,Element elem) /*-{
 		return  new $wnd.Processing(elem,programm);
 	}-*/;
-	
-	
+
+
+    /**
+     * Provides access to the Canvas widget
+     *
+     * @return the {@link Canvas} widget
+     */
 	public Canvas getCanvas() {
 		return canvas;
 	}
--- processingjs-gwt/src/main/java/com/github/timeu/gwtlibs/processingjsgwt/client/ProcessingInstance.java
+++ processingjs-gwt/src/main/java/com/github/timeu/gwtlibs/processingjsgwt/client/ProcessingInstance.java
@@ -3,9 +3,71 @@ package com.github.timeu.gwtlibs.processingjsgwt.client;
 import com.google.gwt.core.client.js.JsType;
 
 
+/**
+ * ProcessingInstance is the base class that is used to interface with the ProcessingJS sketch.
+ * Every ProcessingJS sketch has some default methods (draw, setup, etc)
+ * Users should create their own custom interface extending ProcessingInstance and define the API methods
+ * similar to draw and size. The custom interface must be annotated with {@link @JsType}
+ */
 @JsType
 public interface ProcessingInstance {
 
+	/**
+	 * Called once when the program is started. Used to define initial enviroment properties such as screen size, background color, loading images, etc. before the draw() begins executing.
+	 * Variables declared within setup() are not accessible within other functions, includingdraw().
+     * There can only be one setup() function for each program and it should not be called again after it's initial execution.
+	 */
+	void setup();
+
+	/**
+	 * Defines the dimension of the display window in units of pixels. The size() function must be the first line in setup().
+     * If size() is not called, the default size of the window is 100x100 pixels.
+     * The system variables width and height are set by the parameters passed to the size() function.
+	 *
+	 * @param width width of the sketch
+	 * @param height height of the sketch
+	 */
 	void size(int width,int height);
+
+	/**
+	 * Called directly after setup() and continuously executes the lines of code contained inside its block until the program is stopped or noLoop() is called.
+     * The draw() function is called automatically and should never be called explicitly.
+     * It should always be controlled with noLoop(), redraw() and loop().
+     * After noLoop() stops the code in draw() from executing, redraw() causes the code inside draw() to execute once and loop() will causes the code inside draw() to execute continuously again.
+     * The number of times draw() executes in each second may be controlled with the delay() and frameRate() functions.
+     * There can only be one draw() function for each sketch and draw() must exist if you want the code to run continuously or to process events such as mousePressed().
+     * Sometimes, you might have an empty call to draw() in your program as shown in the above example.
+	 */
 	void draw();
+
+    /**
+     * Executes the code within draw() one time.
+     * This functions allows the program to update the display window only when necessary, for example when an event registered by mousePressed() or keyPressed() occurs.
+     *
+     * In structuring a program, it only makes sense to call redraw() within events such as mousePressed().
+     * This is because redraw() does not run draw() immediately (it only sets a flag that indicates an update is needed).
+     *
+     * Calling redraw() within draw() has no effect because draw() is continuously called anyway.
+     */
+    void redraw();
+
+    /**
+     * Stops Processing from continuously executing the code within draw().
+     * If loop() is called, the code in draw() begin to run continuously again.
+     * If using noLoop() in setup(), it should be the last line inside the block.
+     *
+     * When noLoop() is used, it's not possible to manipulate or access the screen inside event handling functions such as mousePressed() or keyPressed().
+     * Instead, use those functions to call redraw() or loop(), which will run draw(), which can update the screen properly.
+     * This means that when noLoop() has been called, no drawing can happen, and functions like saveFrame() or loadPixels() may not be used.
+     *
+     * Note that if the sketch is resized, redraw() will be called to update the sketch, even after noLoop() has been specified.
+     * Otherwise, the sketch would enter an odd state until loop() was called.
+     */
+    void noLoop();
+
+    /**
+     * Causes Processing to continuously execute the code within draw().
+     * If noLoop() is called, the code in draw() stops executing.
+     */
+    void loop();
 }
